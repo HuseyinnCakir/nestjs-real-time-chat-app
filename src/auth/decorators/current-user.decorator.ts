6c36da15393a11d01c258ea762b0b90b1f984c69
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-04-22 23:47:19 +0300

    Update current-user.decorator.ts

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -4,12 +4,13 @@ import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 
 
-const getCurrentUserByContext = (context: ExecutionContext): User => {
+const getCurrentUserByContext = (context: ExecutionContext): User | undefined => {
   if (context.getType() === 'http') {
     return context.switchToHttp().getRequest().user;
   } else if (context.getType<GqlContextType>() === 'graphql') {
     return GqlExecutionContext.create(context).getContext().req.user;
   }
+  return undefined;
 };
 
 export const CurrentUser = createParamDecorator(
